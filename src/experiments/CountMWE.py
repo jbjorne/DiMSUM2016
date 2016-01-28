497d6cf336db9325906221cd1cafd7f2ff9f937a
jari <jari.bjorne@utu.fi>
2016-01-29 00:02:09 +0200

    Counting the MWEs.

--- src/experiments/CountMWE.py
+++ src/experiments/CountMWE.py
@@ -5,7 +5,12 @@ class CountMWE(Experiment):
         super(CountMWE, self).__init__()
     
     def closeMWE(self, mwe, sentence):
-        self.meta.insert("mwe", {"mwe":"".join(mwe), "sentence":sentence[0]["sentence"], "num_tokens":len(mwe)})
+        self.meta.insert("mwe", {"mwe":"".join([x["MWE"] for x in mwe]), 
+                                 "sentence":sentence[0]["sentence"], 
+                                 "num_tokens":len(mwe),
+                                 "span":" ".join([x["word"] for x in mwe]),
+                                 "text":" ".join([x["lemma"] for x in mwe if x["MWE"] not in ("o", "O")])
+                                 })
         assert mwe in (self.lowerMWE, self.upperMWE)
         if mwe == self.lowerMWE:
             self.lowerMWE = []
@@ -24,15 +29,15 @@ class CountMWE(Experiment):
             if tag in ("B", "b"):
                 if len(mwe) > 0:
                     mwe = self.closeMWE(mwe, sentence)
-                mwe.append(tag)
+                mwe.append(token)
             elif tag in ("I", "i"):
                 assert len(mwe) > 0, token
-                mwe.append(tag)
+                mwe.append(token)
             elif tag == "o":
                 if len(self.upperMWE) > 0:
-                    self.upperMWE.append(tag)
+                    self.upperMWE.append(token)
                 if len(self.lowerMWE) > 0:
-                    self.lowerMWE.append(tag)
+                    self.lowerMWE.append(token)
             else:
                 assert tag == "O", token
                 if self.upperMWE:
