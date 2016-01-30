ac100a1803efd12c63e0be0994b9913dd7e39847
jari <jari.bjorne@utu.fi>
2016-01-30 14:53:33 +0200

    Use resource keys.

--- src/Resources.py
+++ src/Resources.py
@@ -4,24 +4,28 @@ class Resources:
     def __init__(self):
         self.__resources = OrderedDict()
         # Group 1
-        self.__add(1, "labeled training data", "v. 1.5")
-        self.__add(1, "English WordNet")
-        self.__add(1, "ARK TweetNLP clusters")
-        self.__add(1, "clusters: yelpac-c1000-m25.gz")
+        self.__add(1, "corpus", "labeled training data", "v. 1.5")
+        self.__add(1, "WordNet", "English WordNet")
+        self.__add(1, "ARK", "ARK TweetNLP clusters")
+        self.__add(1, "yelpac", "clusters: yelpac-c1000-m25.gz")
         # Group 2
-        self.__add(2, "Yelp Academic Dataset (other than for the above clusters)")
+        self.__add(2, "Yelp", "Yelp Academic Dataset (other than for the above clusters)")
         # Group 3
+    
+    def validate(self, key):
+        if key not in self.__resources:
+            raise Exception("Unknown resource key '" + str(key) + "'")
         
-    def __add(self, level, name, details=None):
+    def __add(self, level, key, name, details=None):
         assert level in (1, 2, 3)
-        assert name not in self.__resources
-        self.__resources[name] = {"level":level, "name":name, "details":details}
+        assert key not in self.__resources
+        self.__resources[key] = {"level":level, "name":name, "details":details}
     
     def __addToReport(self, used, level, report):
         assert level in (1, 2, 3)
         report += "GROUP " + level * "I" + ",,"
-        for name in self.__resources:
-            resource = self.__resources[name]
+        for key in self.__resources:
+            resource = self.__resources[key]
             if resource["level"] == level:
                 s = resource["name"] + ","
                 s += ("Y" if resource["name"] in used else "N") + ","
