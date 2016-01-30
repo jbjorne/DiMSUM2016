0a636b0cc1567366a88a88a66ad10b92a85ec9e7
jari <jari.bjorne@utu.fi>
2016-01-31 00:45:55 +0200

    More work on filtering.

--- src/taggers/WordNetTagger.py
+++ src/taggers/WordNetTagger.py
@@ -22,14 +22,23 @@ class WordNetTagger(Tagger):
     def filterByPOS(self, lexnames, tokens):
         firstPOS = tokens[0]["POS"]
         lastPOS = tokens[0]["POS"]
-        noun
-        if pos in ("NOUN", "PROPN"):
+        numTokens = len(tokens)
+        if firstPOS in self.nounPOS: # Noun
             keep = ("n.",)
-        elif firstPOS in self.verbPOS:
-            keep = "v." if lastPOS not in self.nounPOS else ".n"
+            if lastPOS in self.verbPOS:
+                keep = ("v.", ".n")
+        elif firstPOS in self.verbPOS: # Verb
+            keep = ("v.") 
+            if lastPOS in self.nounPOS:
+                keep = ("v.", ".n")
+        elif numTokens == 1:
+            if firstPOS in ("NUM", "SYM"):
+                keep = ("n.",)
+            if firstPOS in ("ADJ", "ADP", "ADV", "CONJ", "DET", "INTJ", "PART", "PRON", "SCONJ", "X"):
+                return []
         else: # no filtering
             return lexnames
-        return [x for x in lexnames if x[0:2] not in keep]
+        return [x for x in lexnames if x[0:2] in keep]
     
     def tag(self, tokens):
         for key, useLowerCase in [("lemma", False), ("word", True)]:
