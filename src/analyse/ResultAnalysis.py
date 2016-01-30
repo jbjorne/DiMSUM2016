9c7fa2563973955a2cff8bdec439797a603455a3
jari <jari.bjorne@utu.fi>
2016-01-30 23:23:58 +0200

    More reporting for the analysis.

--- src/analyse/ResultAnalysis.py
+++ src/analyse/ResultAnalysis.py
@@ -1,43 +1,51 @@
 import os
 from Analysis import Analysis
 from collections import OrderedDict
+from _collections import defaultdict
 
 class ResultAnalysis(Analysis):
     def __init__(self, dataPath=None):
         super(ResultAnalysis, self).__init__(dataPath=dataPath)
         self.columns = ["index", "word", "lemma", "POS", "MWE", "parent", "strength", "supersense", "sentence"]
     
-    def processSentence(self, sentence):
+    def processSentence(self, sentence, counts):
         for token in sentence:
             sentenceId = token["sentence"]
             index = token["index"]
-            if sentenceId in self.exampleMap and index in self.exampleMap[sentenceId]:
-                examples = self.exampleMap[sentenceId][index]
-                maxExample = None
-                maxPrediction = None
-                for example in examples:
-                    prediction = self.predictions[example["id"]]
-                    if maxPrediction == None or float(prediction["predicted"]) > maxPrediction:
-                        maxExample = example
-                if float(maxPrediction["predicted"]) > 0.0:
-                    token["supersense"] = maxExample["supersense"]
-                    span = sorted([int(x) for x in str(maxExample["tokens"]).split(",")])
-                    assert span[0] == index
-                    if len(span) == 0:
-                        token["MWE"] = "O"
-                    else:
-                        token["MWE"] = "O"
-                        prevIndex = None
-                        for spanIndex in sorted(span):
-                            if prevIndex != None:
-                                assert spanIndex - prevIndex == 1
-                            assert spanIndex >= index
-                            if spanIndex > index:
-                                sentence[spanIndex]["MWE"] = "I"
-                                sentence[spanIndex]["parent"] = spanIndex - 1
-                            else:
-                                token["MWE"] = "B"
-                            prevIndex = spanIndex
+            if sentenceId not in self.exampleMap:
+                continue
+            counts["sentences-with-examples"] += 1
+            if index not in self.exampleMap[sentenceId]:
+                continue
+            counts["tokens-with-examples"] += 1
+            examples = self.exampleMap[sentenceId][index]
+            maxExample = None
+            maxPrediction = None
+            for example in examples:
+                prediction = self.predictions[example["id"]]
+                if maxPrediction == None or float(prediction["predicted"]) > maxPrediction:
+                    maxExample = example
+            if float(maxPrediction["predicted"]) > 0.0:
+                token["supersense"] = maxExample["supersense"]
+                span = sorted([int(x) for x in str(maxExample["tokens"]).split(",")])
+                assert span[0] == index
+                if len(span) == 0:
+                    token["MWE"] = "O"
+                    counts["pred-O"] += 1
+                else:
+                    prevIndex = None
+                    for spanIndex in sorted(span):
+                        if prevIndex != None:
+                            assert spanIndex - prevIndex == 1
+                        assert spanIndex >= index
+                        if spanIndex > index:
+                            sentence[spanIndex]["MWE"] = "I"
+                            sentence[spanIndex]["parent"] = spanIndex - 1
+                            counts["pred-I"] += 1
+                        else:
+                            token["MWE"] = "B"
+                            counts["pred-B"] += 1
+                        prevIndex = spanIndex
     
     def writeTokens(self, tokens, setName):
         print "Processing tokens for dataset '" + setName + "'"
@@ -45,11 +53,12 @@ class ResultAnalysis(Analysis):
         if len(tokens) == 0:
             print "Zero tokens for set", setName
             return
+        counts = defaultdict(int)
         print "Counting sentences"
         sentenceIds = set()
         for token in tokens:
             sentenceIds.add(token["sentence"])
-        numSentences = len(sentenceIds)
+        counts["sentences"] = len(sentenceIds)
         print "Processing sentences"
         sentence = []
         sentenceCount = 0
@@ -58,8 +67,8 @@ class ResultAnalysis(Analysis):
         for token in tokens:
             if prevSentence != None and prevSentence != token["sentence"]:
                 if (sentenceCount + 1) % 100 == 0:
-                    print "Processing sentence", str(sentenceCount + 1) + "/" + str(numSentences)
-                self.processSentence(sentence)
+                    print "Processing sentence", str(sentenceCount + 1) + "/" + str(counts["sentences"])
+                self.processSentence(sentence, counts)
                 for sentenceToken in sentence:
                     outFile.write("\t".join([(str(sentenceToken[column]) if sentenceToken[column] != None else "") for column in self.columns]) + "\n")
                 outFile.write("\n")
@@ -74,6 +83,7 @@ class ResultAnalysis(Analysis):
             sentence.append(outToken)
             
         outFile.close()
+        print "Finished processing dataset '" + setName + "'", counts
         
     def analyse(self, inDir, fileStem=None, hidden=False, clear=True):
         self.inDir = inDir
