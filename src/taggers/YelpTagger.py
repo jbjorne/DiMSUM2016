6f9f24c464e3a9c9f2e1ea1054ca9979b9274b31
jari <jari.bjorne@utu.fi>
2016-02-01 00:11:27 +0200

    A class for the Yelp dataset.

--- src/taggers/YelpTagger.py
+++ src/taggers/YelpTagger.py
@@ -1,6 +1,7 @@
 from Tagger import Tagger
 from src.Meta import Meta
 import os
+from src.yelp.YelpDataset import getYelpDataset
 
 class YelpTagger(Tagger):
     def __init__(self):
@@ -13,29 +14,7 @@ class YelpTagger(Tagger):
     
     def initialize(self, dataPath):
         super(YelpTagger, self).initialize(dataPath)
-        dbPath = os.path.join(dataPath, "yelp", "yelp.sqlite")
-        if not os.path.exists(dbPath):
-            raise Exception("No Yelp database at " + dbPath)
-        self.meta = Meta(dbPath)
-        
-        self.parts = {"middle":{}, "first":{}, "last":{}, "single":{}}
-        for row in self.meta.db.query("select * from part"): # where total > 10;"):
-            pos = row["position"]
-            text = row["token"]
-            if text not in self.parts[pos]:
-                self.parts[pos][text] = []
-            self.parts[pos][text].append(row["category"])
-        
-        self.locations = {}
-        for row in self.meta.db["location"].all():
-            name = row["lower"]
-            if row["lower"] not in self.locations:
-                self.locations[name] = []
-            self.locations[name].append(row)
-        
-        self.names = set([x["lower"] for x in self.meta.db.query("select * from first_name where total > 10;")])
-        for name in ("the",):
-            self.names.remove(name)
+        self.dataset = getYelpDataset(dataPath)
         
     def tag(self, tokens, sentence, taggingState): 
         allUpper, leftUpper, rightUpper = self.getCapitalization(tokens, sentence)
@@ -46,7 +25,7 @@ class YelpTagger(Tagger):
         # Tag personal names
         if len(types) == 0 and len(tokens) <= 2 and allUpper:
             firstName = tokens[0]["word"].lower()
-            if len(firstName) >= 3 and firstName in self.names:
+            if len(firstName) >= 3 and firstName in self.dataset.names:
                 types += ["person"]
         
         # Tag partial matches
@@ -64,8 +43,8 @@ class YelpTagger(Tagger):
         
     def tagExact(self, tokens):        
         text = " ".join([x["word"].lower() for x in tokens])
-        if text in self.locations:
-            return [row["type"] for row in self.locations[text]]
+        if text in self.dataset.locations:
+            return [row["type"] for row in self.dataset.locations[text]]
         return []
     
     def tagPartial(self, tokens, sentence):
@@ -78,14 +57,14 @@ class YelpTagger(Tagger):
         if len(tokens) == 1:
             text = tokens[0]["word"].lower()
             if text in self.parts["single"]:
-                types.extend(self.parts["single"][text])
+                types.extend(self.dataset.parts["single"][text])
         else:
-            last = self.parts["last"].get(tokens[-1]["word"].lower(), [])
+            last = self.dataset.parts["last"].get(tokens[-1]["word"].lower(), [])
             if last:
                 types += last
-                types += self.parts["first"].get(tokens[0]["word"].lower(), [])
+                types += self.dataset.parts["first"].get(tokens[0]["word"].lower(), [])
                 for i in range(1, len(tokens) - 1):
-                    types += self.parts["middle"].get(tokens[i]["word"].lower(), [])
+                    types += self.dataset.parts["middle"].get(tokens[i]["word"].lower(), [])
         if len(types) > 0:
             types += ["business"]
             #print "Match", [x["word"] for x in tokens], types
---
+++ src/yelp/YelpDataset.py
@@ -0,0 +1,39 @@
+import os
+from src.Meta import Meta
+
+YELP_DATASET = None
+
+def getYelpDataset(dataPath):
+    global YELP_DATASET
+    if YELP_DATASET == None:
+        YELP_DATASET = YelpDataset(dataPath)
+    return YELP_DATASET
+
+class YelpDataset():
+    def __init__(self, dataPath):
+        self.dataPath = dataPath
+
+        dbPath = os.path.join(dataPath, "yelp", "yelp.sqlite")
+        if not os.path.exists(dbPath):
+            raise Exception("No Yelp database at " + dbPath)
+        self.meta = Meta(dbPath)
+        
+        self.parts = {"middle":{}, "first":{}, "last":{}, "single":{}}
+        for row in self.meta.db.query("select * from part"): # where total > 10;"):
+            pos = row["position"]
+            text = row["token"]
+            if text not in self.parts[pos]:
+                self.parts[pos][text] = []
+            self.parts[pos][text].append(row["category"])
+        
+        self.locations = {}
+        for row in self.meta.db["location"].all():
+            name = row["lower"]
+            if row["lower"] not in self.locations:
+                self.locations[name] = []
+            self.locations[name].append(row)
+        
+        self.names = set([x["lower"] for x in self.meta.db.query("select * from first_name where total > 10;")])
+        for name in ("the",):
+            self.names.remove(name)
+    
