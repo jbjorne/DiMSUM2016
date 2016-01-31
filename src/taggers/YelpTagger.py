0cb2f0b58218d2e2a63d5b1954e3286515ac422a
jari <jari.bjorne@utu.fi>
2016-01-31 16:21:35 +0200

    Testing Yelp partial matching.

--- src/taggers/YelpTagger.py
+++ src/taggers/YelpTagger.py
@@ -12,17 +12,33 @@ class YelpTagger(Tagger):
         if not os.path.exists(dbPath):
             raise Exception("No Yelp database at " + dbPath)
         self.meta = Meta(dbPath)
-        self.locations = set([row["lower"] for row in self.meta.db["location"].all()])
+        self.parts = {"middle":{}, "first":{}, "last":{}, "single":{}}
+        for row in self.meta.db.query("select * from part where total > 10;"):
+            pos = row["position"]
+            text = row["token"]
+            if text not in self.parts[pos]:
+                self.parts[pos][text] = []
+            self.parts[pos][text].append(row["category"])
+        #self.locations = set([row["lower"] for row in self.meta.db["location"].all()])
     
     def tag(self, tokens, sentence, taggingState):
-        for key, useLowerCase in [("lemma", False), ("word", True)]:
-            text = " ".join([x[key] for x in tokens])
-            if useLowerCase:
-                text = text.lower()
-            if text in self.locations:
-                return ["yelp"]
-            #rows = [x for x in self.meta.db["location"].find(lower=text)]
-            #if len(rows) > 0:
-            #    return ["yelp"]
+        #left, right = self.getFlankingTokens(tokens, sentence)
+        for token in tokens:
+            if token["word"][0].islower():
+                return None
+        categories = []
+        if len(tokens) == 1:
+            text = tokens[0]["word"].lower()
+            if text in self.parts["single"]:
+                categories.extend(self.parts["single"][text])
+        else:
+            last = self.parts["last"].get(tokens[-1]["word"].lower(), [])
+            if last:
+                categories += last
+                categories += self.parts["first"].get(tokens[0]["word"].lower(), [])
+                for i in range(1, len(tokens) - 1):
+                    categories += self.parts["middle"].get(tokens[i]["word"].lower(), [])
+        if len(categories) > 0:
+            return sorted(set(categories))
         return None
         
